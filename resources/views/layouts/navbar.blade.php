f52c04e420e2694279a1e4d929563242ab80346f
unknown <farrel.ahmad.zaidan@gmail.com>
2025-03-22 23:09:04 +0700

    update search page

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 {{-- filepath: d:\Code\KI-Poltek\KI-Poltek\resources\views\layouts\navbar.blade.php --}}
-<div class="navbar bg-[#ffffff] shadow-sm">
+<div class="navbar bg-[#ffffff] shadow-sm absolute top-0 w-full z-50">
     <div class="flex-1 flex items-center m-2">
         <a href="/" class="text-xl flex items-center space-x-2 ml-5">
             <img src="{{ asset('img/logo_polinema.png') }}" alt="Logo" class="h-13 w-auto">
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Search')
 
 @section('content')
-<div class="flex flex-col items-center justify-center min-h-screen space-y-5" style="font-family: 'Montserrat', sans-serif;">
+<div class="flex flex-col items-center justify-center min-h-screen space-y-5 " style="font-family: 'Montserrat', sans-serif;">
     <!-- Title at the Top -->
     <h1 class="text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#2D2E2E] to-[#68C5CC] text-center">
         Halo!
@@ -20,13 +20,13 @@
             placeholder="Cari berdasarkan judul, kategori, atau nomor hak cipta/paten"
             class="input input-bordered w-full bg-white border border-gray-300 rounded-lg px-4 py-2 text-gray-500"
             required>
-        <button type="submit" class="px-6 py-2 rounded-lg text-white bg-[#68C5CC] hover:bg-[#5bb3b8] transition duration-200">
+        <button type="submit" class="px-6 py-2 rounded-lg text-white bg-[#68C5CC] hover:bg-[#5bb3b8] transition duration-200 cursor-pointer">
             Cari
         </button>
     </form>
 
     <!-- Additional Button Below -->
-    <button class="mt-2 px-6 py-2 rounded-lg text-white bg-[#E77817] hover:bg-[#e77817] transition duration-200">
+    <button class="mt-2 px-6 py-2 rounded-lg text-white bg-[#E77817] hover:bg-[#9c5212] transition duration-200 cursor-pointer">
        Pencarian Lanjutan
     </button>
 </div>
