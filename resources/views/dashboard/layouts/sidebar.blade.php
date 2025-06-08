d2dcf5f0847bdbd7d80269504d5877601c0fa739
unknown <farrel.ahmad.zaidan@gmail.com>
2025-06-08 22:14:41 +0700

    update

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="flex h-screen w-16 flex-col justify-between border-e border-gray-100 bg-white sticky top-0 self-start">
+<div class="flex h-screen w-16 flex-col justify-between border-e border-gray-100 bg-white sticky top-0 self-start"> {{-- Re-added h-screen --}}
   <div>
     <div class="inline-flex size-16 items-center justify-center">
       {{-- Replaced "KI" text with the Polinema logo image --}}
@@ -145,7 +145,7 @@
           {{-- Unggah Data: Paten Sentra --}}
           <li>
             <a
-              href="{{ route('dashboard.paten.index') }}" {{-- Changed from create to index --}}
+              href="{{ route('dashboard.paten.index') }}"
               class="group relative flex justify-center rounded-sm px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
             >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 opacity-75">
@@ -154,7 +154,7 @@
               <span
                 class="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded-sm bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
               >
-                Daftar Paten {{-- Changed text from Unggah Paten to Daftar Paten --}}
+                Unggah Paten
               </span>
             </a>
           </li>
@@ -164,33 +164,30 @@
   </div>
 
   <div class="sticky inset-x-0 bottom-0 border-t border-gray-100 bg-white p-2">
-    {{-- Logout Form --}}
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit"
-            class="group relative flex w-full justify-center rounded-lg px-2 py-1.5 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="size-5 opacity-75"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                />
-            </svg>
+    <a
+      href="{{ route('logout') }}" {{-- Assuming logout is a POST route, you'll need a form for this --}}
+      class="group relative flex w-full justify-center rounded-lg px-2 py-1.5 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="size-5 opacity-75"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+        />
+      </svg>
 
-            <span
-                class="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded-sm bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
-            >
-                Logout
-            </span>
-        </button>
-    </form>
+      <span
+        class="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded-sm bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+      >
+        Logout
+      </span>
+    </a>
   </div>
 </div>
